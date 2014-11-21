7651b25e1c435d2c03bb320185637f56e456b344
Noah <noah.yonack@gmail.com>
2014-11-21 13:15:16 -0500

    python wrapper for Instagram

---
+++ python_wrapper/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="python-instagram",
+      version="1.2.0",
+      description="Instagram API client",
+      license="MIT",
+      install_requires=["simplejson","httplib2","six"],
+      author="Instagram, Inc",
+      author_email="apidevelopers@instagram.com",
+      url="http://github.com/Instagram/python-instagram",
+      packages = find_packages(),
+      keywords= "instagram",
+      zip_safe = True)
