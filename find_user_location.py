93eb2b1a3377bb41aff6b94f4c8dd60cc2bee2b9
Noah <noah.yonack@gmail.com>
2014-11-21 19:45:42 -0500

    filtering via geocode api

--- find_user_location.py
+++ find_user_location.py
@@ -28,30 +28,30 @@ def construct_location_data(user):
 	except:
 		return "You don't have permission to follow user with id: " + str(user)
 
-def sanitize_locations(locations):
-	""" consolidates (lat, lng) tuples based on a threshold """
-	def average_location_list(locations):
-		""" averages a list of location tuples """
-		return tuple(map(lambda elt: sum(elt) / float(len(elt)), zip(*locations)))
-	def is_location_nearby(element, pivot, threshold):
-		""" checks if a location is near another location based on threshold """
-		lat_difference = abs(pivot[0] - element[0])
-		lng_difference = abs(pivot[1] - element[1])
-		within_threshold = lat_difference < threshold and lng_difference < threshold
-		return True if within_threshold else False
-	master = []
-	def filter_locations():
-		nearby = []
-		nearby.append(locations[0])
-		for location in locations:
-			if is_location_nearby(location, locations[0], THRESHOLD):
-				nearby.append(location)
-				locations.remove(location)
-		master.append(average_location_list(nearby))
-		if not locations == []:
-			filter_locations()
-	filter_locations()
-	return master
+# def sanitize_locations(locations):
+# 	""" consolidates (lat, lng) tuples based on a threshold """
+# 	def average_location_list(locations):
+# 		""" averages a list of location tuples """
+# 		return tuple(map(lambda elt: sum(elt) / float(len(elt)), zip(*locations)))
+# 	def is_location_nearby(element, pivot, threshold):
+# 		""" checks if a location is near another location based on threshold """
+# 		lat_difference = abs(pivot[0] - element[0])
+# 		lng_difference = abs(pivot[1] - element[1])
+# 		within_threshold = lat_difference < threshold and lng_difference < threshold
+# 		return True if within_threshold else False
+# 	master = []
+# 	def filter_locations():
+# 		nearby = []
+# 		nearby.append(locations[0])
+# 		for location in locations:
+# 			if is_location_nearby(location, locations[0], THRESHOLD):
+# 				nearby.append(location)
+# 				locations.remove(location)
+# 		master.append(average_location_list(nearby))
+# 		if not locations == []:
+# 			filter_locations()
+# 	filter_locations()
+# 	return master
 
 def determine_city(clusters):
 	cities = []
@@ -68,13 +68,21 @@ def determine_city(clusters):
 			address_data = response.read()
 			data = json.loads(address_data)
 			city = data["results"][0]["formatted_address"].split(",")[-3]
-			if not city in cities:
-				print city
-				cities.append(city)
+			city = str(city).strip()
+			if not any(city in locations for locations in cities):
+				cities.append((city, 1))
+			else:
+				tupleIndex = [index for index, location in enumerate(cities) if location[0] == city]
+				city = list(cities[tupleIndex[0]])
+				city[1] += 1
+				updated_count = tuple(city)
+				cities.remove(cities[tupleIndex[0]])
+				cities.append(updated_count)
 		except:
 		    pass
+	print cities
 
 if __name__ == "__main__":
 	locations = construct_location_data(23983519)
-	clusters = sanitize_locations(locations)
-	determine_city(clusters)
+	# clusters = sanitize_locations(locations)
+	determine_city(locations)
