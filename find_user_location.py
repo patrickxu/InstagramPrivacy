793bf1e71ccb160f81aeb8441fcb3105a1e48f95
Noah <noah.yonack@gmail.com>
2014-11-22 22:35:51 -0500

    successfully finding user's home

--- find_user_location.py
+++ find_user_location.py
@@ -21,22 +21,23 @@ def find_users_we_follow():
 			ids.append(user.id.encode('utf8'))
 	return ids
 
-# def construct_location_data(user):
-# 	""" constructs every possible (lat, long) tuple for a given user """
-# 	location_list = []
-# 	try:
-# 		recent_media, next_ = api.user_recent_media(user_id=user, count=sys.maxint)
-# 		for index, media in enumerate(recent_media):
-# 		   	# print "Caption " + str(index) + ": ", media.caption.text
-# 			try:
-# 		   		latitude = media.location.point.latitude
-# 		   		longitude = media.location.point.longitude
-# 		   		location_list.append((latitude, longitude))
-# 			except:
-# 				pass
-# 		return location_list
-# 	except:
-# 		return "You don't have permission to follow user with id: " + str(user)
+
+def construct_location_data(user):
+	""" constructs every possible (lat, long) tuple for a given user """
+	location_list = []
+	try:
+		recent_media, next_ = api.user_recent_media(user_id=user, count=sys.maxint)
+		for index, media in enumerate(recent_media):
+		   	# print "Caption " + str(index) + ": ", media.caption.text
+			try:
+		   		latitude = media.location.point.latitude
+		   		longitude = media.location.point.longitude
+		   		location_list.append((latitude, longitude))
+			except:
+				pass
+		return location_list
+	except:
+		return "You don't have permission to follow user with id: " + str(user)
 
 # def average_location_list(locations):
 # 	""" averages a list of location tuples """
@@ -90,40 +91,49 @@ def find_users_we_follow():
 # 	# 		master.append(location)
 # 	# return master
 
-# def determine_city(clusters):
-# 	cities = []
-# 	for location in clusters:
-# 		lat = str(location[0])
-# 		lng = str(location[1])
-# 		key = "AIzaSyCyjoz5Fhu26s6B6GK6k5ImZxMAkSdxL6E"
-# 		request = Request("https://maps.googleapis.com/maps/api/geocode/json?latlng="
-# 			+ lat
-# 			+ ","
-# 			+ lng
-# 			+ "&key="
-# 			+ key)
-# 		try:
-# 			response = urlopen(request)
-# 			address_data = response.read()
-# 			data = json.loads(address_data)
-# 			city = data["results"][0]["formatted_address"].split(",")[-3]
-# 			city = str(city).strip()
-# 			""" TODO: super inefficient way of grouping cities because
-# 				it hits google's endpoint far too many times """
-# 			if not any(city in locations for locations in cities):
-# 				cities.append((city, 1))
-# 			else:
-# 				tupleIndex = [index for index, location in enumerate(cities) if location[0] == city]
-# 				city = list(cities[tupleIndex[0]])
-# 				city[1] += 1
-# 				updated_count = tuple(city)
-# 				cities.remove(cities[tupleIndex[0]])
-# 				cities.append(updated_count)
-# 		except:
-# 		    pass
-# 	print cities
+def find_frequented_cities(clusters):
+	cities = []
+	for location in clusters:
+		lat = str(location[0])
+		lng = str(location[1])
+		key = "AIzaSyCyjoz5Fhu26s6B6GK6k5ImZxMAkSdxL6E"
+		request = Request("https://maps.googleapis.com/maps/api/geocode/json?latlng="
+			+ lat
+			+ ","
+			+ lng
+			+ "&key="
+			+ key)
+		try:
+			response = urlopen(request)
+			address_data = response.read()
+			data = json.loads(address_data)
+			city = data["results"][0]["formatted_address"].split(",")[-3]
+			city = str(city).strip()
+			""" TODO: super inefficient way of grouping cities because
+				it hits google's endpoint far too many times """
+			if not any(city in locations for locations in cities):
+				cities.append((city, 1))
+			else:
+				tupleIndex = [index for index, location in enumerate(cities) if location[0] == city]
+				city = list(cities[tupleIndex[0]])
+				city[1] += 1
+				updated_count = tuple(city)
+				cities.remove(cities[tupleIndex[0]])
+				cities.append(updated_count)
+		except:
+		    pass
+	return cities
 
-# if __name__ == "__main__":
-# 	locations = construct_location_data(52449842)
-# 	#clusters = sanitize_locations(locations)
-# 	determine_city(locations)
+def find_home(location_list):
+	max_city = max(location_list, key=itemgetter(1))[0]
+	max_occurences = max(location_list, key=itemgetter(1))[1]
+	if max_city == "Cambridge":
+		location_list.remove((max_city, max_occurences))
+		return max(location_list, key=itemgetter(1))[0]
+	else:
+		return max_city
+
+if __name__ == "__main__":
+	locations = construct_location_data(52449842)
+	#clusters = sanitize_locations(locations)
+	print find_home(find_frequented_cities(locations))
