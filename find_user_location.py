38714c014c385f4f77ffed0e7d998d8088d4a499
Noah <noah.yonack@gmail.com>
2014-11-25 16:45:42 -0600

    cleaning up code...safe to revert

--- find_user_location.py
+++ find_user_location.py
@@ -96,34 +96,33 @@ def create_clusters(list_of_coordinates):
 		cluster_lists.append(cluster(list_of_coordinates))
 	return cluster_lists
 
-"""
-	Parameters: Takes in a lat-long coordinate
-	Function: Uses API to return city, state for lat-long coordinate
-	Returns: City, state string
-"""
+# returns a (city, state) tuple based on a (lat, lng) coordinate
 def query_city(location):
+	""" uses Google's Geocode API to convert (lat, lng) tuples
+		into readable addresses """
 	lat = str(location[0])
 	lng = str(location[1])
-	#NoahKey
-	#key = "AIzaSyCyjoz5Fhu26s6B6GK6k5ImZxMAkSdxL6E"
+	# key = "AIzaSyCyjoz5Fhu26s6B6GK6k5ImZxMAkSdxL6E"
 	key = "AIzaSyB7ZZTzG8-hhXKpCbuYbRinFb18mrMV55c"
-	#key = "AIzaSyAaJpkAmGeVO1qrmTb-qGrDCRAJxs8NjTk"
-	request = Request("https://maps.googleapis.com/maps/api/geocode/json?latlng="
-		+ lat
-		+ ","
-		+ lng
-		# added locality for easier parsing
-		+ "&result_type=locality"
-		+ "&key="
-		+ key)
+	# key = "AIzaSyAaJpkAmGeVO1qrmTb-qGrDCRAJxs8NjTk"
 	try:
+		# hit the geocode endpoint
+		request = Request("https://maps.googleapis.com/maps/api/geocode/json?latlng="
+						+ lat
+						+ ","
+						+ lng
+						+ "&result_type=locality"
+						+ "&key="
+						+ key)
+		# parse the response into JSON
 		response = urlopen(request)
 		address_data = response.read()
 		data = json.loads(address_data)
+		# parse the JSON into a city and state
 		results = data["results"][0]["formatted_address"]
 		components = data["results"][0]["address_components"]
-		# pprint(components)
 		city = components[0]["long_name"]
+		# figure out where to index the state
 		if components[-1]["short_name"] == u'US':
 			# state = ' '.join(results.split(",")[-2].strip().split(" ")[:-1])
 			# city = results.split(",")[-3]
@@ -141,20 +140,20 @@ def query_city(location):
 		return whole
 	except Exception as e:
 		print "Can't get city from tuple..." + str(e)
-		# pass
 
-"""
-	Parameters: list of lists, with each list being a cluster
-	Function: Takes each "cluster", calculates (city, state) associated with cluster 
-				and the number of pictures associated with the location
-	Returns: dictionary with (city, state) keys, and number of occurrences for value
-"""
+# returns a dictionary with (city, state) keys and location frequency as value
 def construct_city_mapping(list_of_clusters):
+	""" constructs a dictionary that maps the most frequented 
+		location (besides Cambridge) to the number of tags in that
+		locations """
 	city_map = {}
+	# biggest sublist implies most frequented location
 	max_city = max(list_of_clusters, key = len)
 	max_city_occurences = len(max_city)
 	max_city_average = average_location_list(max_city)
+	# query based on average for best guess
 	city_name = query_city(max_city_average)
+	# if Cambridge, remove from list of clusters and re-guess
 	if city_name in ["Cambridge, MA", "Mid-Cambridge, MA"]:
 		list_of_clusters.remove(max_city)
 		second_max_city = max(list_of_clusters, key = len)
@@ -165,34 +164,12 @@ def construct_city_mapping(list_of_clusters):
 	else:
 		city_map[city_name] = max_city_occurences
 	return city_map
-	# for cluster in list_of_clusters:
-	# 	average_location = average_location_list(cluster)
-	# 	city_name = query_city(average_location)
-	# 	num_occurences = len(cluster)
-	# 	city_map[city_name] = num_occurences
-	# return city_map
 
-"""
-	Parameters: Takes a dictionary that has location keys and number of occurrences values
-	Function: Calculates the city with most occurrences that is not Cambridge, MA
-	Returns: string of city with most occurrences that is not Cambridge, MA
-"""
-def find_user_home(city_map):
-	max_city = max(city_map.iteritems(), key=itemgetter(1))[0]
-	max_occurences = max(city_map.iteritems(), key=itemgetter(1))[1]
-	if max_city in ["Cambridge, MA", "Mid-Cambridge, MA"]:
-		del city_map[max_city]
-		return max(city_map.iteritems(), key=itemgetter(1))[0]
-	else:
-		return max_city
-
-"""
-	Parameters: list of ids and a filename
-	Function: Calculates the assumed hometown for each of these users, and writes all username-hometown tuples to a txt file
-	Returns: Dictionary that has username as key, and hometown as value.
-"""
+# returns a dictionary that maps username to home
 def construct_id_home_mapping(idlist, f):
+	""" guesses the hometown for each user and writes info to a txt file """
 	mapping = {}
+	# attempts to make a home guess for each user
 	for user_tuple in idlist:
 		# print "Attemping to construct map for user ", user_tuple, "..."
 		try:
@@ -200,14 +177,9 @@ def construct_id_home_mapping(idlist, f):
 			locations = construct_location_data(userid)
 			clusters = create_clusters(locations)
 			city_map = construct_city_mapping(clusters)
-			# home = find_user_home(city_map)
 			print user_tuple[1], city_map.keys()[0]
 			to_write = user_tuple[1] + '\t' + city_map.keys()[0] + '\n'
 			f.write(to_write)
-			# coordinates = construct_location_data(user)
-			# unique_locations = find_frequented_cities(coordinates)
-			# home = find_user_home(unique_locations)
-			# mapping[user] = home
 		except Exception as e:
 			print "Master error..." + str(e)
 			# pass
