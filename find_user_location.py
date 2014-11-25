dbfb620791d56a3826369a61c35bacd170e55571
patrickxu <patrickxu@college.harvard.edu>
2014-11-25 03:02:56 -0500

    patrick's first commits
    -changed clustering algorithm
    -changed file to write to excel file
    -included states to the cities to better identify places
    -changed the code to account for international addresses, which have to
    be indexed differently

--- find_user_location.py
+++ find_user_location.py
@@ -12,6 +12,8 @@ CLIENT_ID = 1570583193
 access_token = open("token.txt").read()
 api = InstagramAPI(access_token=access_token)
 
+# use Instagram API to get all the users tom_hammons follows
+# RETURNS: list of ids we follow
 def find_users_we_follow():
 	ids = []
 	followed_users = api.user_follows(CLIENT_ID, as_generator=True)
@@ -40,15 +42,18 @@ def construct_location_data(user):
 		else:
 			return location_list
 	except Exception as e:
-		# return "You don't have permissions to view media from user with id: " + user + "\n" + str(e)
-		pass
+		return "You don't have permissions to view media from user with id: " + user + "\n" + str(e)
+		# pass
 
 def is_location_nearby(element1, element2, threshold):
 	""" checks if a location is near another location based on threshold """
 	lat_difference = abs(element2[0] - element1[0])
 	lng_difference = abs(element2[1] - element1[1])
 	within_threshold = lat_difference < threshold and lng_difference < threshold
-	return True if within_threshold else False
+	if within_threshold:
+		return True
+	else:
+		return False
 
 def is_location_in_array(element, coordinates, threshold):
 	unique = False
@@ -59,18 +64,24 @@ def is_location_in_array(element, coordinates, threshold):
 
 def cluster(list_of_coordinates):
 	clustered_list = []
-	clustered_list.insert(0,[list_of_coordinates[0]])
+	clustered_list.append(list_of_coordinates[0])
 	list_of_coordinates.remove(list_of_coordinates[0])
-	for pair in list_of_coordinates:
-		if is_location_in_array(pair, clustered_list[0], THRESHOLD):
-			clustered_list[0].append(pair)
-			list_of_coordinates.remove(pair)
+	count = 0
+	while count < len(list_of_coordinates):
+		pair = list_of_coordinates[count]
+		average_location = average_location_list(clustered_list)
+		# if is_location_in_array(pair, clustered_list[0], THRESHOLD):
+		if is_location_nearby(pair, average_location, THRESHOLD):
+			clustered_list.append(pair)
+			del list_of_coordinates[count]
+		else:
+			count = count + 1
 	return clustered_list
 
 def create_clusters(list_of_coordinates):
 	master = []
 	while (not list_of_coordinates == []):
-		master.append(cluster(list_of_coordinates)[0])
+		master.append(cluster(list_of_coordinates))
 	return master
 
 def average_location_list(locations):
@@ -80,7 +91,10 @@ def average_location_list(locations):
 def query_city(location):
 	lat = str(location[0])
 	lng = str(location[1])
-	key = "AIzaSyCyjoz5Fhu26s6B6GK6k5ImZxMAkSdxL6E"
+	#NoahKey
+	#key = "AIzaSyCyjoz5Fhu26s6B6GK6k5ImZxMAkSdxL6E"
+	# key = "AIzaSyB7ZZTzG8-hhXKpCbuYbRinFb18mrMV55c"
+	key = "AIzaSyAaJpkAmGeVO1qrmTb-qGrDCRAJxs8NjTk"
 	request = Request("https://maps.googleapis.com/maps/api/geocode/json?latlng="
 		+ lat
 		+ ","
@@ -91,12 +105,19 @@ def query_city(location):
 		response = urlopen(request)
 		address_data = response.read()
 		data = json.loads(address_data)
-		city = data["results"][0]["formatted_address"].split(",")[-3]
-		city = str(city).strip()
+		results = data["results"][0]["formatted_address"]
+		if ', USA' in results:
+			state = ' '.join(results.split(",")[-2].strip().split(" ")[:-1])
+			city = results.split(",")[-3]
+		else:
+			split_res = results.split(',')
+			state = split_res[-1]
+			city = split_res[-2]
+		city = str(city).strip() + ', ' + state.strip()
 		return city
 	except Exception as e:
-		# print "Can't get city from tuple..." + str(e)
-		pass
+		print "Can't get city from tuple..." + str(e)
+		# pass
 
 def construct_city_mapping(list_of_clusters):
 	city_map = {}
@@ -110,16 +131,16 @@ def construct_city_mapping(list_of_clusters):
 def find_user_home(city_map):
 	max_city = max(city_map.iteritems(), key=itemgetter(1))[0]
 	max_occurences = max(city_map.iteritems(), key=itemgetter(1))[1]
-	if max_city in ["Cambridge", "Mid-Cambridge"]:
+	if max_city in ["Cambridge, MA", "Mid-Cambridge, MA"]:
 		del city_map[max_city]
 		return max(city_map.iteritems(), key=itemgetter(1))[0]
 	else:
 		return max_city
 
-def construct_id_home_mapping(idlist):
+def construct_id_home_mapping(idlist, f):
 	mapping = {}
 	for user_tuple in idlist:
-		print "Attemping to construct map for user ", user_tuple, "..."
+		# print "Attemping to construct map for user ", user_tuple, "..."
 		try:
 			userid = user_tuple[0]
 			locations = construct_location_data(userid)
@@ -127,15 +148,28 @@ def construct_id_home_mapping(idlist):
 			city_map = construct_city_mapping(clusters)
 			home = find_user_home(city_map)
 			print user_tuple[1], home
+			to_write = user_tuple[1] + '\t' + home + '\n'
+			f.write(to_write)
 			# coordinates = construct_location_data(user)
 			# unique_locations = find_frequented_cities(coordinates)
 			# home = find_user_home(unique_locations)
 			# mapping[user] = home
 		except Exception as e:
-			# print "Master error..." + str(e)
-			pass
+			print "Master error..." + str(e)
+			# pass
 	return mapping
 
 if __name__ == "__main__":
+	# check = [(42.375097093, -71.115406025), (42.375097093, -71.115406025), (42.37446105, -71.120158211), (42.374981758, -71.115754964), (42.375273924, -71.115778573), (42.375149103, -71.115834927), (37.619562741, -122.385499283), (42.375440123, -71.115521276), (42.375274299, -71.115690174)]
+	# print len(create_clusters(check))
+	f = open('user_hometowns.txt', 'w')
+	f.write('username\thometown\n')
+	construct_id_home_mapping(['415272494'], f)
 	idlist = find_users_we_follow()
-	construct_id_home_mapping(idlist)
+	construct_id_home_mapping(idlist[:4], f)
+	f.close()
+
+
+
+
+
