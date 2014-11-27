c80949f2b18dc46cccea57ca191fbeddb89609b7
Noah <noah.yonack@gmail.com>
2014-11-26 20:28:05 -0600

    successfully querying thanksgiving location

--- update_thanksgiving_loc.py
+++ update_thanksgiving_loc.py
@@ -1,7 +1,7 @@
 import datetime
 from instagram.client import InstagramAPI
 import csv
-from find_user_location import find_users_we_follow, construct_location_data
+from find_user_location import query_city, average_location_list
 
 # authenticate with IG
 access_token = open("token.txt").read()
@@ -18,19 +18,54 @@ def collect_csv_ids(file_):
 		ids.append(row["userid"])
 	return ids
 
-def get_recent_media(idlist):
-	for id_ in idlist:
-		recent_media, next_ = IGapi.user_recent_media(user_id=id_, count=5)
-		for media in recent_media:
-			datetime = media.created_time
-			year = int(datetime.strftime("%Y"))
-			month = int(datetime.strftime("%m"))
-			day = int(datetime.strftime("%d"))
-			# these are the posts for which we can compare locations
-			is_thanksgiving_post = year > 2013 and month > 10 and day > 23
-			print is_thanksgiving_post
-			
+# returns a list of media posted during thanksgiving vacation
+def get_recent_media(id_):
+	""" loops through recent media and checks against a start date """
+	location_list = []
+	recent_media, next_ = IGapi.user_recent_media(user_id=id_, count=5)
+	for media in recent_media:
+		datetime = media.created_time
+		# strip the relevant date information
+		year = int(datetime.strftime("%Y"))
+		month = int(datetime.strftime("%m"))
+		day = int(datetime.strftime("%d"))
+		# these are the posts for which we can compare locations
+		is_thanksgiving_post = year > 2013 and month > 10 and day > 23
+		if is_thanksgiving_post:
+			try:
+		   		latitude = media.location.point.latitude
+		   		longitude = media.location.point.longitude
+	   			location_list.append((latitude, longitude))
+			except Exception as e:
+				pass
+	return (id_, average_location_list(location_list))
 
+# returns a santized mapping from user to thanksgiving location
+def sanitize_recent_media(file_):
+	""" removes empty location data (e.g. [] or ()) """
+	master = []
+	for id_ in collect_csv_ids(file_):
+		if get_recent_media(id_)[1]:
+			master.append(get_recent_media(id_))
+	return master
 
-idlist = collect_csv_ids("hometowns.csv")
-get_recent_media(idlist)
+# returns a mapping from userid to thanksgiving location
+def find_thanksgiving_address(mapping):
+	""" changes (lat, lng) associated with user to readable address """
+	city_mapping = {}
+	for tuple_ in mapping:
+		list_ = list(tuple_)
+		list_[1] = query_city(list_[1])
+		new_tuple = tuple(list_)
+		id_ = new_tuple[0]
+		address = str(new_tuple[1])
+		city_mapping[id_] = address
+	return city_mapping
+
+def write_updated_location(city_mapping, file):
+	""" writes the thanksgiving location to a new column in the csv """
+	# code code code
+
+if __name__ == "__main__":
+	mapping = sanitize_recent_media("hometowns_to_test.csv")
+	print find_thanksgiving_address(mapping)
